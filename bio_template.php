7ab8ec53d407dcd01a08433d0f6f55d850754654
Tyler Simon <simont@purdue.edu>
2019-03-22 14:34:32 -0400

    Bio and Contact

---
+++ bio_template.php
@@ -0,0 +1,122 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PRPC - Rifle Captain</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+<div class="navbar">
+    <img src="images/rpc.png" alt="PRPC Logo">
+    <h1>Purdue Rifle and Pistol Club</h1>
+
+    <ul class="social-icons">
+        <li><i class="fab fa-facebook-f" aria-hidden="true"></i></li>
+        <li><i class="fab fa-instagram" aria-hidden="true"></i></li>
+        <li><i class="fab fa-twitter" aria-hidden="true"></i></li>
+    </ul>
+
+    <button>Donate</button>
+
+    <nav role="navigation" class="navmenu">
+        <a href="rifle.php" class="navlink">Rifle</a>
+        <a href="pistol.php" class="navlink">Pistol</a>
+        <a href="about.php" class="navlink">About</a>
+        <a href="gallery.php" class="navlink">Gallery</a>
+        <a href="contact.php" class="navlink">Contact</a>
+    </nav>
+</div>
+
+<div class="container">
+    <div class="bioHeader">
+        <p id="memberName">Amanda Smith</p>
+        <p id="memberRole">Rifle Captain</p>
+
+        <button>Back to Rifle Page</button>
+    </div>
+    <div class="bioText">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas rutrum massa quis neque rhoncus molestie nec id neque. Praesent sodales tincidunt nisl non tempor. Nulla ac lobortis urna. Vivamus varius lacus at libero imperdiet, id gravida nulla porta. Sed non est non est tempor finibus non nec mi. Phasellus id dui lectus. Integer sed vulputate nunc. Suspendisse potenti. Etiam vehicula tellus a orci tempus, sit amet pulvinar nisl fringilla. Sed suscipit, tortor quis sagittis pellentesque, dolor massa aliquet dui, quis luctus nisl metus a nisl. Sed vel pellentesque nunc. Suspendisse dui erat, lobortis sed facilisis at, auctor vel mi. Phasellus a arcu porttitor, placerat nulla vitae, interdum massa. Sed tristique mi ut lorem volutpat, pharetra pretium magna varius. Sed at sollicitudin massa, vitae mattis justo.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec nibh lacus. Mauris non est nisi. Quisque id felis porta, venenatis mi et, interdum neque. In hac habitasse platea dictumst. Nunc euismod libero non imperdiet tincidunt. Integer elementum facilisis felis vel dignissim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt risus ex, ut semper sapien vulputate nec. Nullam tempus arcu in mattis molestie. Suspendisse tempus augue eget lacus vehicula sollicitudin. Aliquam erat volutpat. Maecenas viverra orci et feugiat suscipit. Fusce velit risus, dignissim et tortor ac, vulputate ornare neque. Nulla ac nisl finibus, aliquet nisi a, molestie neque. Donec tempor purus at aliquam euismod.</p>
+
+        <img src="images/prpc-3-XL.jpg" alt="Close Up">
+    </div>
+    <div class="bioRecords">
+        <img src="images/prpc-13-XL.jpg" alt="Wide Shot">
+
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="2">Personal Records</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Smallbore (Offhand):</td>
+                    <td>562</td>
+                </tr>
+                <tr>
+                    <td>Smallbore (Prone):</td>
+                    <td>789</td>
+                </tr>
+                <tr>
+                    <td>Smallbore (Kneeling):</td>
+                    <td>672</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table>
+            <thead>
+            <tr>
+                <th colspan="2">Achievements</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>8th Rifle Nationals</td>
+                <td>2017</td>
+            </tr>
+            <tr>
+                <td>2nd Rifle Smallbore (Kneeling) Sectionals</td>
+                <td>2018</td>
+            </tr>
+            <tr>
+                <td>1st Rifle Conference</td>
+                <td>2019</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<div class="footer">
+    <div class="sponsors">
+        <h3>Sponsors</h3>
+        <div class="sponsorImages">
+            <img src="images/nwtf.png" alt="NWTF Logo">
+            <img src="images/Friends-of-NRA.png" alt="Friends of NRA Logo">
+            <img src="images/NRA_Foundation.png" alt="NRA Logo">
+            <img src="images/rpc.png" alt="PRPC Logo">
+            <img src="images/midway_logo.png" alt="Midway Foundation Logo">
+        </div>
+    </div>
+    <div class="bottomBar">
+        <p>Copyright &copy; Purdue Rifle and Pistol Club</p>
+        <ul class="social-icons">
+            <li><i class="fab fa-facebook-f" aria-hidden="true"></i></li>
+            <li><i class="fab fa-instagram" aria-hidden="true"></i></li>
+            <li><i class="fab fa-twitter" aria-hidden="true"></i></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,111 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>PRPC - Contact</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+    <div class="navbar">
+        <img src="images/rpc.png" alt="PRPC Logo">
+        <h1>Purdue Rifle and Pistol Club</h1>
+
+        <ul class="social-icons">
+            <li><i class="fab fa-facebook-f" aria-hidden="true"></i></li>
+            <li><i class="fab fa-instagram" aria-hidden="true"></i></li>
+            <li><i class="fab fa-twitter" aria-hidden="true"></i></li>
+        </ul>
+
+        <button>Donate</button>
+
+        <nav role="navigation" class="navmenu">
+            <a href="rifle.php" class="navlink">Rifle</a>
+            <a href="pistol.php" class="navlink">Pistol</a>
+            <a href="about.php" class="navlink">About</a>
+            <a href="gallery.php" class="navlink">Gallery</a>
+            <a href="contact.php" class="navlink">Contact</a>
+        </nav>
+    </div>
+
+    <div class="container">
+        <img src="images/JDR_2348.jpg" alt="Group Photo">
+
+        <div class="contactContainer">
+            <h2>Contact</h2>
+
+            <div class="contactInfoContainer">
+                <div class="contactInfoTop">
+                    <ul>
+                        <li>Club President</li>
+                        <li>Katie Morris</li>
+                        <li>email@email.com</li>
+                        <li>(123) 456-7890</li>
+                    </ul>
+                    <ul>
+                        <li>Club Advisor</li>
+                        <li>Mike Reckowsky</li>
+                        <li>email@email.com</li>
+                        <li>(123) 456-7890</li>
+                    </ul>
+                </div>
+                <div class="contactInfoBottom">
+                    <ul>
+                        <li>Rifle Coach</li>
+                        <li>Keith Adams</li>
+                        <li>email@email.com</li>
+                        <li>(123) 456-7890</li>
+                    </ul>
+                    <ul>
+                        <li>Pistol Coach</li>
+                        <li>Aaron Lorton</li>
+                        <li>email@email.com</li>
+                        <li>(123) 456-7890</li>
+                    </ul>
+                </div>
+            </div>
+
+            <h3>Location</h3>
+            <div class="locationContainer">
+                <img src="images/map.PNG" alt="Facility Location">
+                <ul>
+                    <li>Purdue Armory</li>
+                    <li>812 3rd Street, West Lafayette, IN, 47907</li>
+                </ul>
+                <button>Get Directions</button>
+                <button>Facilities</button>
+            </div>
+
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FPurdueRPC%2F&tabs=messages&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+        </div>
+    </div>
+
+    <div class="footer">
+        <div class="sponsors">
+            <h3>Sponsors</h3>
+            <div class="sponsorImages">
+                <img src="images/nwtf.png" alt="NWTF Logo">
+                <img src="images/Friends-of-NRA.png" alt="Friends of NRA Logo">
+                <img src="images/NRA_Foundation.png" alt="NRA Logo">
+                <img src="images/rpc.png" alt="PRPC Logo">
+                <img src="images/midway_logo.png" alt="Midway Foundation Logo">
+            </div>
+        </div>
+        <div class="bottomBar">
+            <p>Copyright &copy; Purdue Rifle and Pistol Club</p>
+            <ul class="social-icons">
+                <li><i class="fab fa-facebook-f" aria-hidden="true"></i></li>
+                <li><i class="fab fa-instagram" aria-hidden="true"></i></li>
+                <li><i class="fab fa-twitter" aria-hidden="true"></i></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
